660f32d55daf6847552b1aacbe9bfef0e1830523
sumit <sumit@sumit.ml>
2026-03-20 16:15:06 +0900

    updated load inline

--- utils/load_inline.py
+++ utils/load_inline.py
@@ -1,8 +1,16 @@
 from torch.utils.cpp_extension import load_inline
 
+add_cpp = """
+#include <iostream>
+int add(int a, int b) {
+    return a + b;
+}
+"""
+
+
 my_module = load_inline(
     name="my_module",
-    cpp_sources=["kernels/add/add.cpp"],
+    cpp_sources=[add_cpp],
     functions="add",
     verbose=True,
 )
