7bd68669e67d28a9e73daec0f85b07b5d0fd940f
sumit <sumit@sumit.ml>
2026-03-20 18:24:41 +0900

    pmpp, 2.8

--- pmpp/add.cu
+++ pmpp/add.cu
@@ -1,19 +1,24 @@
-void vecAdd(float *A, float *B, float *C, int n) {
+void vecAdd(float *A_h, float *B_h, float *C_h, int n) {
   int size = n * sizeof(float);
-  float *d_A, *d_B, *d_C;
+  float *A_d, *B_d, *C_d;
   // Part 1: Allocate device memory for A, B, and C
   // Copy A and B to device memory
 
-  cudaMalloc((void **)&d_A, size);
-  cudaMalloc((void **)&d_B, size);
-  cudaMalloc((void **)&d_C, size);
+  cudaMalloc((void **)&A_d, size);
+  cudaMalloc((void **)&B_d, size);
+  cudaMalloc((void **)&C_d, size);
+
+  cudaMemcpy(A_d, A_h, size, cudaMemcpyHostToDevice);
+  cudaMemcpy(B_d, B_h, size, cudaMemcpyHostToDevice);
 
   // Part 2: Call kernel – to launch a grid of threads
   // to perform the actual vector addition
 
   // Part 3: Copy C from the device memory
+  cudaMemcpy(C_h, C_d, size, cudaMemcpyDeviceToHost);
+
   // Free device vectors
-  cudaFree(d_A);
-  cudaFree(d_B);
-  cudaFree(d_C);
+  cudaFree(A_d);
+  cudaFree(B_d);
+  cudaFree(C_d);
 }
