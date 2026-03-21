b0ca98ee2792310eef0ae11216619e2edbeef171
sumit <sumit@sumit.ml>
2026-03-22 02:12:33 +0900

    updated vecAdd into a runnable program with host setup and verification

--- pmpp/add.cu
+++ pmpp/add.cu
@@ -1,13 +1,26 @@
 #include <cmath>
 #include <stdio.h>
+#include <stdlib.h>
 
-void vecAddKernel(float *A, float *B, float *C, int n);
+__global__ void vecAddKernel(float *A, float *B, float *C, int n);
 
-int vecAdd(float *A_h, float *B_h, float *C_h, int n) {
+int main() {
+  int n = 1 << 20; // 1M elements
   int size = n * sizeof(float);
+
+  // memory allocation on host
+  float *A_h = (float *)malloc(size);
+  float *B_h = (float *)malloc(size);
+  float *C_h = (float *)malloc(size);
+
+  // filling up these with dummies
+  for (int i = 0; i < n; ++i) {
+    A_h[i] = 1.0f;
+    B_h[i] = 2.0f;
+  }
+
+  // device pointers declaration
   float *A_d, *B_d, *C_d;
-  // Part 1: Allocate device memory for A, B, and C
-  // Copy A and B to device memory
 
   cudaMalloc((void **)&A_d, size);
   cudaMalloc((void **)&B_d, size);
@@ -16,17 +29,26 @@ int vecAdd(float *A_h, float *B_h, float *C_h, int n) {
   cudaMemcpy(A_d, A_h, size, cudaMemcpyHostToDevice);
   cudaMemcpy(B_d, B_h, size, cudaMemcpyHostToDevice);
 
-  // Part 2: Call kernel – to launch a grid of threads
-  // to perform the actual vector addition
+  // calling the kernel to launch a grid of threads to perform vector addition
   vecAddKernel<<<ceil(n / 256.0), 256>>>(A_d, B_d, C_d, n);
 
-  // Part 3: Copy C from the device memory
+  // copying c from the device memory
   cudaMemcpy(C_h, C_d, size, cudaMemcpyDeviceToHost);
 
-  // Free device vectors
   cudaFree(A_d);
   cudaFree(B_d);
   cudaFree(C_d);
+
+  printf("Printing the first 20 results\n");
+  for (int i = 0; i < 20; ++i) {
+    printf("C_h at %i: %f\n", i, C_h[i]);
+  }
+
+  free(A_h);
+  free(B_h);
+  free(C_h);
+
+  return 0;
 }
 
 __global__ void vecAddKernel(float *A, float *B, float *C, int n) {
